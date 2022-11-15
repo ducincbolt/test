34ae37b061c7058d396613c650c4c84a4f39a01e
0xankylosaurus <duc.minh@autonomous.nyc>
2022-11-15 05:35:06 +0000

    Update transfer script

--- transfernstake.sh
+++ transfernstake.sh
@@ -9,19 +9,19 @@ while [ $# -gt 0 ]; do
 done
 
 # transfer 
-for (( i = 0; i < $max; i++ ))
+for (( i = $min; i < $max; i++ ))
 do
 	pa=$(jq -r .Accounts[$i].PaymentAddress $keychainpath)
 	echo "Processing transfer for index: $i,  address: $pa"
-	#./cli --cache 1 --debug 1 send --privateKey $prikey --address $pa --amount 1750000000400
-	sleep 2 # 30s
+	./cli --cache 1 --debug 1 send --privateKey $prikey --address $pa --amount 1750000000400
+	sleep 30 # 30s
 done
 
 # stake
-for (( i = 0; i < $max; i++ ))
+for (( i = $min; i < $max; i++ ))
 do
 	privateKey=$(jq -r .Accounts[$i].PrivateKey $keychainpath)
 	echo "Processing staking for index: $i"
-	#./cli --cache 1 stake --privateKey $privateKey --rewardAddress $rewardaddr --autoReStake 1
-	sleep 1 # 10s
+	./cli --cache 1 stake --privateKey $privateKey --rewardAddress $rewardaddr --autoReStake 1
+	sleep 10 # 10s
 done
